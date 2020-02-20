cecc556dbe6c5edec3f4129bc24e6b37b7ca1ae6
ahmadmamdouh <mamdouh486@gmail.com>
2020-02-20 11:56:41 +0200

    add multibly

--- app.js
+++ app.js
@@ -31,8 +31,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+    var theMul = a*b;
+    // The product of 5 and 9 is 45.
+    var message = `The product of ${a} and ${b} is ${theMul}.`;
+    return [theMul,message];
 }
+testMultiply(5,9) ;
+
+
 
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
